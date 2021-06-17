72a2295f6f88efa95673ecf894d247a3a328e7cc
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-06-16 23:24:57 -0400

    elemao

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-var ivoox = require('node-ivoox');
+const express = require("express");
+const app = express();
+const ivoox = require('node-ivoox');
 
-ivoox.audios("https://www.ivoox.com/podcast-viaje-al-mundo-del-jazz_sq_f11234707_1.html").then(function(data) { console.log(data) }).catch(function(e) { console.error(e); });
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    ivoox.audios("https://www.ivoox.com/podcast-viaje-al-mundo-del-jazz_sq_f11234707_1.html").then(function(data) { res.send(data) }).catch(function(e) { console.error(e); });
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
