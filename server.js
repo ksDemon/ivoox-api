a8f48577682a4800f4a73dc7c25cb338cbcadf10
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-07-09 17:05:06 -0400

    pages

--- server.js
+++ server.js
@@ -7,10 +7,33 @@ app.use(cors());
 
 let port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    ivoox.audios("https://www.ivoox.com/podcast-viaje-al-mundo-del-jazz_sq_f11234707_1.html").then(function(data) { res.send(data) }).catch(function(e) { console.error(e); });
-})
+var allData = []
+var page = 1
+var prev = 0
+
+readPages(page)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
+function readPages(page){
+    ivoox.audios(`https://www.ivoox.com/podcast-viaje-al-mundo-del-jazz_sq_f11234707_${page}.html`).then(function(data) {
+        prev = allData.length
+            addData(data);
+            if (prev == allData.length){
+                app.get("/", (req, res) => {
+                    res.send(allData)
+                })
+            }
+            else {
+                page = page + 1;
+                readPages(page)
+            } 
+        });
+}
+
+function addData(data){
+    allData = allData.concat(data);
+    return allData;
+}
