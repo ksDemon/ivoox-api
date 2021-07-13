6e2271adbd2f8a3c14c31425047451dbd2525f32
ksDemon <64107178+ksDemon@users.noreply.github.com>
2021-07-13 14:03:38 -0400

    only first page

--- server.js
+++ server.js
@@ -7,33 +7,9 @@ app.use(cors());
 
 let port = process.env.PORT || 3000;
 
-var allData = []
-var page = 1
-var prev = 0
-
-readPages(page)
-
+app.get("/", (req, res) => {
+    ivoox.audios("https://www.ivoox.com/podcast-viaje-al-mundo-del-jazz_sq_f11234707_1.html").then(function(data) { res.send(data) }).catch(function(e) { console.error(e); });
+})
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
-
-function readPages(page){
-    ivoox.audios(`https://www.ivoox.com/podcast-viaje-al-mundo-del-jazz_sq_f11234707_${page}.html`).then(function(data) {
-        prev = allData.length
-            addData(data);
-            if (prev == allData.length){
-                app.get("/", (req, res) => {
-                    res.send(allData)
-                })
-            }
-            else {
-                page = page + 1;
-                readPages(page)
-            } 
-        });
-}
-
-function addData(data){
-    allData = allData.concat(data);
-    return allData;
-}
